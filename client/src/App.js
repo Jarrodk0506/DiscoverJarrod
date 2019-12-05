4afeea2b74fadaeb3bfca3336d814e3365b82aed
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-12-05 12:16:30 -0500

    working on styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import github from "./github.png"
+import resume from "./resume.jpg"
 import Divtest from "../src/components/wrapper/wrapper"
 
+
 class App extends Component {
+
+  handleGitClick = e => {
+    e.preventDefault();
+    console.log("clicked");
+    }
+
+    
   render() {
     return (
       <div className="App">
@@ -11,12 +21,14 @@ class App extends Component {
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h2 className="discover">Discover Jarrod</h2>
+            <button id="linkButton" onClick={this.handleGitClick}><strong class="text">Resume</strong></button>
+            <button id="linkButton"><img onClick={this.handleGitClick} id="linkImage" src={github} alt="github" href="https://github.com/Jarrodk0506" target="_blank"/></button>
           </div>
-          <Divtest></Divtest>
+            <Divtest></Divtest>
+          </div >
         </div >
-      </div >
-    );
-  }
-}
-
-export default App;
+        );
+      }
+    }
+    
+    export default App;
