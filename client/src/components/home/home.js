d8a1ad9c2cc8c5aee062011e09a8f21f5b4ba483
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-12-27 11:46:14 -0500

    positioning content

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -6,16 +6,17 @@ class Home extends Component {
   render() {
     return (
       <div className="contentWrap">
-            {/* <div className="picturediv">
-              <img className="picture" src={picture}></img>
-            </div> */}
-            <div className="content">
-              <h4>Hi, My name is Jarrod Krasnoff and I am a</h4>
-              <h1>Junior Full-Stack Web-Developer</h1>
-              <button className="glow-on-hover" type="button">About me</button>
-              <button className="glow-on-hover" type="button">Portfolio</button>
-            </div>
+        <div className="content">
+          <div className="title">
+            <h5>Hi, My name is Jarrod Krasnoff and I am a</h5>
+            <h1>Junior Full-Stack Web-Developer</h1>
           </div>
+          <div className="buttons">
+            <button className="glow-on-hover" type="button">About me</button>
+            <button className="glow-on-hover" type="button">Portfolio</button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
