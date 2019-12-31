4914353b4213c49332f4ae898d7329027efad1be
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-12-31 15:43:29 -0500

    styling portfolio with project pictures, working carousel

--- client/src/components/portfolio/portfolio.js
+++ client/src/components/portfolio/portfolio.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import "./portfolio.css";
+import shortcutz from "../assets/shortcutz.png"
+import kodex from "../assets/kodex.JPG"
+import trivia from "../assets/trivia.JPG"
+import wordGuess from "../assets/wordGuess.JPG"
+import train from "../assets/train.JPG"
+import noms from "../assets/noms.JPG"
 
 class Portfolio extends Component {
 
@@ -20,7 +26,46 @@ class Portfolio extends Component {
             <div className="contentWrap">
                 <div className="content">
                     <div className="jumbotron">
-                        <h1>This is a portfolio</h1>
+                        <div className="jumboContent">
+                            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                                <ol className="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                                </ol>
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img src={kodex} className="d-block w-100" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={shortcutz} className="d-block w-100" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={trivia} className="d-block w-100" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={wordGuess} className="d-block w-100" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={train} className="d-block w-100" alt="..." />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src={noms} className="d-block w-100" alt="..." />
+                                    </div>
+                                </div>
+                                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                     <div className="portfolioButtons">
                         <button className="glow-on-hover" onClick={this.home} type="button">Home</button>
