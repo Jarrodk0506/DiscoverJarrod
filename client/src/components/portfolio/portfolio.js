8fa0d36a3e2bb954281733077affb0f4cad00300
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2020-01-13 15:03:15 -0500

    update descriptions on portfolio

--- client/src/components/portfolio/portfolio.js
+++ client/src/components/portfolio/portfolio.js
@@ -4,7 +4,6 @@ import shortcutz from "../assets/shortcutz.png"
 import kodex from "../assets/kodex.JPG"
 import trivia from "../assets/trivia.JPG"
 import wordGuess from "../assets/wordGuess.JPG"
-import train from "../assets/train.JPG"
 import noms from "../assets/noms.JPG"
 
 class Portfolio extends Component {
@@ -34,13 +33,12 @@ class Portfolio extends Component {
                                     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                                     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
                                     <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
                                 </ol>
                                 <div className="carousel-inner">
                                     <div className="carousel-item active">
                                         <a href=" https://k0dex.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={kodex} className="d-block w-100" alt="..." /></a>
                                         <div className="carousel-text">
-                                            <p>Kodex, a platform where code experts could get together and help each other fix problems with their code. Get ideas and find better practices. Create and edit your profile. Add your skills to the profile. Find top trending topics and what's new in the programing community Members can create posts where people from all over the nation can help solve problems with your code. </p><p>Technologies used: <ul className="techUsed"><li>Node.js</li><li>Express</li><li>MySQL</li><li>Passport-local</li> <li>Bcrypt</li><li> Handlebar</li></ul></p>
+                                            <p>Kodex</p><p> a platform where code experts could get together and help each other fix problems with their code. Get ideas and find better practices. Create and edit your profile. Add your skills to the profile. Find top trending topics and what's new in the programing community Members can create posts where people from all over the nation can help solve problems with your code. </p><p>Technologies used: <ul className="techUsed"><li>Node.js</li><li>Express</li><li>MySQL</li><li>Passport-local</li> <li>Bcrypt</li><li> Handlebar</li></ul></p>
                                         </div>
                                     </div>
                                     <div className="carousel-item">
@@ -52,19 +50,13 @@ class Portfolio extends Component {
                                     <div className="carousel-item">
                                         <a href="https://jarrodk0506.github.io/Trivia-game/" target="_blank" rel="noopener noreferrer"><img src={trivia} className="d-block w-100" alt="..." /></a>
                                         <div className="carousel-text">
-                                            <p>Trivia</p>
+                                            <p>90's Themed Nickelodeon Trivia game!</p><p>This is a fun quick project to learn how to use setTimeOut function. Try and Get all the questions correct! Fun trivia from every 90's Nickelodeon cartoons I loved from "aaahh!!! Real Monsters" to "Rugrats". You only get 10 seconds per question so do your best and good luck!</p>
                                         </div>
                                     </div>
                                     <div className="carousel-item">
                                         <a href="https://jarrodk0506.github.io/Word-Guess-Game/" target="_blank" rel="noopener noreferrer"><img src={wordGuess} className="d-block w-100" alt="..." /></a>
                                         <div className="carousel-text">
-                                            <p>word-guess-game</p>
-                                        </div>
-                                    </div>
-                                    <div className="carousel-item">
-                                        <a href="https://jarrodk0506.github.io/Train-Schedule/" target="_blank" rel="noopener noreferrer"><img src={train} className="d-block w-100" alt="..." /></a>
-                                        <div className="carousel-text">
-                                            <p>Train</p>
+                                            <p>Planetary Word-Guess-Game</p><p> Type the letter you guess one by one to guess what is the mystery planet! Watch as the game replaces the dashes with correctly guessed letters! Letters guess incorrectly go into the letters guessed section. Game will count your wins and how many tries you have left. You only get 10 tries, Good luck! P.s. Enjoy an instrumental if you'd please with the play button</p>
                                         </div>
                                     </div>
                                     <div className="carousel-item">
